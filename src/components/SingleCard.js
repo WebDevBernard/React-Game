c394935bf5bda0210f69740bf3c54269eb5ae356
Bernard Yang <ybernard@gmail.com>
2022-01-24 19:14:19 -0800

    Update SingleCard.js

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -9,7 +9,7 @@ export default function SingleCard({ card, handleChoice, flipped, disabled }) {
   return (
     <div className="card">
       <div className={flipped ? "flipped" : ""}>
-        <img className="front" src={card.src} alt="card front" />
+        <img className="front" src={card.src} alt={card.name} />
         <img
           className="back"
           src="/img/pokeball.png"
