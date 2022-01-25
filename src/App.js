64f3815b5cbec870762fd1ce84064e8baf8e346b
Bernard Yang <ybernard@gmail.com>
2022-01-24 16:21:34 -0800

    finish netninja tutorial

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ const cardImages = [
 
 function App() {
   const [cards, setCards] = useState([]);
-  const [turns, setTurns] = useState([]);
+  const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
   // randomizes cards
   const shuffleCards = () => {
@@ -25,7 +26,10 @@ function App() {
       // returns the new array of the sorted array
       .map((card) => ({ ...card, id: Math.random() }));
 
+    setChoiceOne(null);
+    setChoiceTwo(null);
     setCards(shuffledCards);
+    setTurns(0);
   };
 
   // handle a choice
@@ -40,11 +44,13 @@ function App() {
     setChoiceOne(null);
     setChoiceTwo(null);
     setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
   };
   // compare 2 selected cards
 
   useEffect(() => {
     if (choiceOne && choiceTwo) {
+      setDisabled(true);
       if (choiceOne.src === choiceTwo.src) {
         setCards((prevCards) => {
           return prevCards.map((card) => {
@@ -57,13 +63,17 @@ function App() {
         });
         resetTurn();
       } else {
-        console.log("those cards do not match");
-        resetTurn();
+        setTimeout(() => resetTurn(), 1000);
       }
     }
   }, [choiceOne, choiceTwo]);
 
-  console.log(cards);
+  // starts game automatically
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -75,9 +85,11 @@ function App() {
             card={card}
             handleChoice={handleChoice}
             flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,14 @@
 import "./SingleCard.css";
 
-export default function SingleCard({ card, handleChoice, flipped }) {
+export default function SingleCard({ card, handleChoice, flipped, disabled }) {
   const handleClick = () => {
-    handleChoice(card);
+    if (!disabled) {
+      handleChoice(card);
+    }
   };
   return (
     <div className="card">
-      <div className={flipped && "flipped"}>
+      <div className={flipped ? "flipped" : ""}>
         <img className="front" src={card.src} alt="card front" />
         <img
           className="back"
